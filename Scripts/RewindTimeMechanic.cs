9f4d3ce8463d2d4629fa5ada8225f30f0e975826
Eren <erencan_inanci@hotmail.com>
2021-02-10 03:09:42 +0300

    Update RewindTimeMechanic.cs

--- Scripts/RewindTimeMechanic.cs
+++ Scripts/RewindTimeMechanic.cs
@@ -4,7 +4,6 @@ using UnityEngine;
 
 public class BackinTimeMechanics : MonoBehaviour
 {
-
     [SerializeField] private bool isRewinding = false;
     List<PointInTime> pointsInTime;
     Rigidbody rb;
